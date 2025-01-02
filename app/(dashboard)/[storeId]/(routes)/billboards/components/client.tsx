657df55d8974a57d9ba6d6072f6949ee161752f1
Salah Abimhamed <sabimhamed@gmail.com>
2025-01-02 17:58:14 +0300

    API Path for Billboards

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -6,8 +6,11 @@ import { useParams, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
 
-import { BillboardColumn } from "./columns";
+import { BillboardColumn, columns } from "./columns";
+import { ApiList } from "@/components/ui/api-list";
+
 
 
 interface BillboardClientProps {
@@ -32,6 +35,10 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
         </Button>
         </div>
         <Separator />
+        <DataTable searchKey="label" columns={columns} data={data}/>
+        <Heading title="API" description="API calls for billboards for your store"/>
+        <Separator />
+        <ApiList entityName="billboards" entityIdName="billboardId"/>
     </>
     
   );
