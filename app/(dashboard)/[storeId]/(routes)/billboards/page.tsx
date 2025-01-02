49f0a1c568a9ee528ad77b7b0ab67b3ed8abbfbf
Salah Abimhamed <sabimhamed@gmail.com>
2025-01-02 17:58:57 +0300

    api billboads and delete option

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -11,19 +11,25 @@ const BillboardsPage = async ({
 }: {
   params: { storeId: string;};
 }) => {
+
+  const { storeId } = await params;
   const billboards = await prismadb.billboard.findMany({
     where: {
-      storeId: params.storeId,
+      storeId: storeId,
     },
+
     orderBy: {
       createdAt: 'desc',
-  }});
+  }
+});
 
   const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
     id: item.id,
     label: item.label,
     createdAt: format( item.createdAt, 'MMMM do, yyyy' ),
-  }));
+  }
+));
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
